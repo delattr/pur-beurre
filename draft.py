fc6ec23385523615cf452863dff9b41ec7d8cb7c
elenoko <elenoko@gmail.com>
2018-06-02 16:56:40 +0200

    load json to dict objects and save them into MySQL tables.

--- draft.py
+++ draft.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 
 import requests
-import openfoodfacts
+import getpass
 import json
 import pprint
 import pymysql.cursors
@@ -22,90 +22,154 @@ class Login:
 
         try:
 
-            cursor = self.connection.cursor()
+            #Create database 'openfoodfacts'
+            with self.connection.cursor() as cursor:
 
-            sql_db = ('CREATE DATABASE IF NOT EXISTS `Openfoodfacts`')
+                cursor.execute('drop database openfoodfacts')
 
+                sql = ('CREATE DATABASE IF NOT EXISTS Openfoodfacts')
+                cursor.execute(sql)
+                self.connection.commit()
 
-            cursor.execute(sql_db)
+            #Create talbe 'categories'
+            with self.connection.cursor() as cursor:
+                sql =('CREATE TABLE IF NOT EXISTS Openfoodfacts.categories ('
+                        'id SMALLINT UNSIGNED AUTO_INCREMENT, '
+                        'category VARCHAR(50), '
+                        'PRIMARY KEY (id)'
+                     ') ENGINE=InnoDB')
+                cursor.execute(sql)
+                self.connection.commit()
 
+            #Create TABLE 'products'
+            with self.connection.cursor() as cursor:
+                sql =('CREATE TABLE IF NOT EXISTS Openfoodfacts.Products ('
+                        'id SMALLINT UNSIGNED AUTO_INCREMENT, '
+                        'code BIGINT UNSIGNED NOT NULL, '
+                        'brands VARCHAR(100), '
+                        'product_name VARCHAR(200), '
+                        'nutrition_grade_fr varchar(1), '
+                        'stores VARCHAR(90), '
+                        'quantity VARCHAR(100), '
+                        'url VARCHAR(500), '
+                        'cat_id SMALLINT UNSIGNED, '
+                        'PRIMARY KEY (id), '
+                        'CONSTRAINT fk_cat_id FOREIGN KEY (cat_id) REFERENCES Openfoodfacts.categories(id)'
+                     ') ENGINE=InnoDB')
 
-            self.connection.commit()
-
-
-            sql_tb = ('CREATE TABLE IF NOT EXISTS Openfoodfacts.Products ('
-                      'id smallint UNSIGNED PRIMARY KEY, '
-                      'code INT(13) UNSIGNED UNIQUE, '
-                      'brands VARCHAR(20), '
-                      'product_name VARCHAR(40), '
-                      'categories TEXT, '
-                      'nutrition_grade_fr VARCHAR(1), '
-                      'nutrition_score_fr SMALLINT, '
-                      'stores VARCHAR(10), '
-                      'quantity VARCHAR(10), '
-                      'url TEXT) ENGINE=InnoDB')
-
-            cursor.execute(sql_tb)
-            self.connection.commit()
-
+                cursor.execute(sql)
+                self.connection.commit()
 
         finally:
             pass
 
-    def insert(self, data):
-        self.data = data
-
-        columns = ['code','brands','product_name', 'categories',
-                   'nutrition_grade_fr', 'store',
-                   'quantity', 'url']
+    def insert_categories(self, category, cat_id):
+        self.category = category
+        self.cat_id = cat_id
 
         try:
 
-            cursor = self.connection.cursor()
+            with self.connection.cursor() as cursor:
 
-            for column_name in columns:
-                for item in self.data['products']:
+                sql =('INSERT INTO Openfoodfacts.Categories (id, category) VALUES (%s, %s)')
+                cursor.execute(sql, (self.cat_id, self.category))
 
-                    sql = ('INSERT INTO Openfoodfacts.Products ({}) '.format(column_name) +
-                           'VALUES ({})'.format(item[column_name]))
+        finally:
+            pass
 
 
-            cursor.execute(sql)
-            self.connection.commit()
+
+    def insert_products(self, data):
+
+        self.data = data
+
+
+        try:
+
+            with self.connection.cursor() as cursor:
+
+                for product in self.data:
+                    placeholder = ','.join(['%s'] * len(product))
+                    columns = ','.join(product.keys())
+
+                    sql =('INSERT INTO Openfoodfacts.Products ({}) VALUES ({})'
+                            .format(columns, placeholder))
+
+                    cursor.execute(sql,(list(product.values())))
+                    self.connection.commit()
 
         finally:
             pass
 
 class Request:
 
-    def __init__(self, category):
+
+    def __init__(self, category, cat_id):
 
         self.category = category
+        self.cat_id = cat_id
         self.data = []
 
 
     def get_request(self):
 
-        url = 'https://fr.openfoodfacts.org/category/{}.json'
-        r = requests.get(url.format(self.category))
+        # url = 'https://fr.openfoodfacts.org/category/{}.json&lc=fr'
+        # r = requests.get(url.format(self.category))
+        #
+        # #load json
+        # json = r.json()
 
-        #load json
-        self.data = r.json()
+        payload = {'action': 'process',
+                   'tagtype_0': 'categories',
+                   'tag_contains_0': 'contains',
+                   'tag_0': self.category,
+                   'tagtype_1': 'countries',
+                   'tag_contains_1': 'contains',
+                   'tag_1' :'france',
+                   'sort_by' : 'unique_scans_n',
+                   'page_size':100,
+                   'json': 1}
 
 
 
+        address = 'https://fr.openfoodfacts.org/cgi/search.pl'
+        r = requests.get(address, params=payload)
+        json = r.json()
+
+        columns = ['code', 'brands', 'product_name',
+                   'nutrition_grade_fr','stores',
+                   'quantity', 'url']
+
+        data_category = []
+        data_product = []
+        #pprint.pprint(sorted(json['products'][0].keys()))
 
 
-user = input('user: ')
-password = input('password: ')
+        for item in json['products']:
 
-database = Login(user, password)
+            mydict = {k:item[k] for k in columns if k in item if item[k] !=''}
 
-print('Initializing...')
+            mydict['cat_id'] = self.cat_id
+            data_category.append(mydict)
+        self.data = data_category
 
+
+#user = input('user: ')
+# password = getpass.getpass()
+categories =['pizza','pain']
+
+database = Login('mayfly', 'password')
 database.create_db()
 
-pizza = Request('pizza')
-pizza.get_request()
 
-database.insert(pizza.data)
+
+
+for item in categories:
+    cat_id = categories.index(item) + 1
+    #Request class instance
+    cat = Request(item, cat_id)
+    cat.get_request()
+
+    #execute class Login methodes
+    database.insert_categories(item, cat_id)
+    database.insert_products(cat.data)
