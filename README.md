572183a7815ddb31cd91581962ec27033a3e9a9b
elenoko <elenoko@gmail.com>
2018-06-12 19:30:20 +0200

    requests api

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
 # Pur Beurre
 
-Pur Beuure helps users to make healthier food choices by
+**Pur Beure** helps users to make healthier food choices by
 recommending foods with higher nutrition score.
 
-## Prerequisites
-- python 3
+This is a project number 05 of Python developer course from
+[OPENCLASSROOMS](https://openclassrooms.com/)
+
+## Geting started
+
+### Prerequisites
+- python 3.6.5
 - PyMysql 0.8.1
-- requests 2.11.1
-- MySQL 5.x (MySQL 8 does not fucntion properly with PyMysql)
+- Requests 2.11.1
+- MySQL 5.7
+
+### Installing
+Running under a vertual environment is recommended.
+
+1. Connect to MySQL and grant privileges.
+```Mysql
+GRANT ALL ON openfoodfacts.* TO 'user'@'localhost';
+```
+2. Install requirements.txt using pip install.
+```
+pip install -r requirements.txt
+```
+3. Run pur_beurre.py
+
+## API Reference
+[API openfoodfacts](https://en.wiki.openfoodfacts.org/API)
+
+## Supported Language
+French
---
+++ api_requests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import json
+import pprint
+
+class Requests:
+
+    def get_json(self, category):
+        self.category = category
+
+        payload = {'action': 'process',
+                   'fields': 'code,brands,product_name,nutrition_grade_fr,\
+                              stores,quantity,ingredients_text,url',
+                   'tagtype_0': 'categories',
+                   'tag_contains_0': 'contains',
+                   'tag_0': self.category,
+                   'tagtype_1': 'countries',
+                   'tag_contains_1': 'contains',
+                   'tag_1' :'france',
+                   'sort_by' : 'unique_scans_n',
+                   'page_size':100,
+                   'json': 1}
+
+
+
+        address = 'https://fr.openfoodfacts.org/cgi/search.pl'
+        r = requests.get(address, params=payload)
+        return r.json()
+
+j=Requests()
+cat=j.get_json('pizza')
+
+f = open('json.txt', 'w', encoding='utf-8')
+f.write(str(cat))
+f.close
